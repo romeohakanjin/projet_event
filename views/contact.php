f9741084eaf13ff9125c597d7482c790a2f60c1e
romeohakanjin <hakanjin.romeo@hotmail.fr>
2016-11-10 14:08:47 +0100

    Update contact.php

--- views/contact.php
+++ views/contact.php
@@ -1,7 +1,7 @@
 <h1>Contact</h1>
 <?php foreach (Contact::getContact() as $contact) : ?>	
 	<h2><?php print $contact->nom.' '.$contact->prenom ?></h2>
-	<p><?php print $contact->adresse.', '.$contact->code_postal.' - '.$contact->ville ?></p>
+	<p><?php print $contact->adresse.', '.$contact->ville ?></p>
 	<hr>
 	<br/>
 <?php endforeach; ?>
