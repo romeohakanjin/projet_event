6bc26f0871d8aea405ecf8fcce0d364b5cc92ae1
Sindy-L <sindy.lim@outlook.fr>
2016-11-22 12:20:18 +0100

    maj bdd et avancement inscription

--- app/database/project_event.sql
+++ app/database/project_event.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 17 Novembre 2016 à 19:35
+-- Généré le :  Mar 22 Novembre 2016 à 11:10
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -50,69 +50,25 @@ CREATE TABLE `membre` (
   `id` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
   `date_naissance` date NOT NULL,
   `adresse` varchar(50) NOT NULL,
   `code_postal` varchar(20) NOT NULL,
   `ville` varchar(250) NOT NULL,
   `type_contrat` varchar(50) NOT NULL,
-  `id_utilisateur` int(11) DEFAULT NULL,
-  `id_type_membre` int(11) DEFAULT '1',
   `id_etat_inscription` int(11) DEFAULT '1',
   `civilite` enum('Mme','Mr') DEFAULT NULL,
-  `niveau_etude` enum('1','2','3','4','5','autre') DEFAULT NULL
+  `niveau_etude` enum('1','2','3','4','5','autre') DEFAULT NULL,
+  `date_inscription` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `membre`
 --
 
-INSERT INTO `membre` (`id`, `nom`, `prenom`, `date_naissance`, `adresse`, `code_postal`, `ville`, `type_contrat`, `id_utilisateur`, `id_type_membre`, `id_etat_inscription`, `civilite`, `niveau_etude`) VALUES
-(1, 'sindy', 'testPrenom', '1996-11-29 00:00:00', 'testAdresse', 12345, 'testVille', 'Contrat pro', 3, 2, 2, 'Mme', 5),
-(2, 'testNom2', 'testPrenom2', '1995-11-06 00:00:00', 'testAdresse2', 57456, 'testVille2', 'Contrat pro', 2, 2, 2, 'Mr', 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `type_membre`
---
-
-CREATE TABLE `type_membre` (
-  `id` int(11) NOT NULL,
-  `libelle` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `type_membre`
---
-
-INSERT INTO `type_membre` (`id`, `libelle`) VALUES
-(1, 'Administrateur'),
-(2, 'Candidat');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `utilisateur`
---
-
-CREATE TABLE `utilisateur` (
-  `id` int(11) NOT NULL,
-  `identifiant` varchar(20) NOT NULL,
-  `mot_de_passe` varchar(50) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `date_inscription` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `id_membre` int(11) DEFAULT NULL,
-  `id_type_membre` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Contenu de la table `utilisateur`
---
-
-INSERT INTO `utilisateur` (`id`, `identifiant`, `mot_de_passe`, `email`, `date_inscription`, `id_membre`, `id_type_membre`) VALUES
-(1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin@test.com', '2016-11-27 00:00:00', NULL, 1 ),
-(2, 'test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'test@test.com', '2016-11-14 00:00:00', 1, 2),
-(3, 'sindy', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'sindy@test.com', '2016-11-17 20:22:33', 2, 2);
+INSERT INTO `membre` (`id`, `nom`, `prenom`, `email`, `date_naissance`, `adresse`, `code_postal`, `ville`, `type_contrat`, `id_etat_inscription`, `civilite`, `niveau_etude`, `date_inscription`) VALUES
+(1, 'sindy', 'testPrenom', 'sindy@test.com', '1996-11-29', 'testAdresse', '12345', 'testVille', 'Contrat pro', 2, 'Mme', '5', '2016-11-22 12:04:36'),
+(2, 'testNom2', 'testPrenom2', 'test@test.com', '1995-11-06', 'testAdresse2', '57456', 'testVille2', 'Contrat pro', 2, 'Mr', '1', '2016-11-22 12:04:36');
 
 --
 -- Index pour les tables exportées
@@ -129,23 +85,7 @@ ALTER TABLE `etat_inscription`
 --
 ALTER TABLE `membre`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FKM1` (`id_utilisateur`),
-  ADD KEY `FKM2` (`id_type_membre`),
   ADD KEY `FKM3` (`id_etat_inscription`);
-
---
--- Index pour la table `type_membre`
---
-ALTER TABLE `type_membre`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FKU1` (`id_membre`),
-  ADD KEY `FKU2` (`id_type_membre`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -162,16 +102,6 @@ ALTER TABLE `etat_inscription`
 ALTER TABLE `membre`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT pour la table `type_membre`
---
-ALTER TABLE `type_membre`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
 -- Contraintes pour les tables exportées
 --
 
@@ -179,16 +109,7 @@ ALTER TABLE `utilisateur`
 -- Contraintes pour la table `membre`
 --
 ALTER TABLE `membre`
-  ADD CONSTRAINT `FKM1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`),
-  ADD CONSTRAINT `FKM2` FOREIGN KEY (`id_type_membre`) REFERENCES `type_membre` (`id`),
   ADD CONSTRAINT `FKM3` FOREIGN KEY (`id_etat_inscription`) REFERENCES `etat_inscription` (`id`);
-
---
--- Contraintes pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD CONSTRAINT `FKU1` FOREIGN KEY (`id_membre`) REFERENCES `membre` (`id`),
-  ADD CONSTRAINT `FKU2` FOREIGN KEY (`id_type_membre`) REFERENCES `type_membre` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- views/users/inscription.php
+++ views/users/inscription.php
@@ -1,25 +1,48 @@
 <h2>Inscription</h2>
+<br />
 <div class="container">
   <form class="form-signin" action="../controllers/index.php?action=inscription" method="POST">
 
-    <label for="inputEmail" class="sr-only">Email address</label>
+    <label for="inputEmail" class="sr-only">Email</label>
     <input autocomplete="off" type="email" name="inputEmail" id="inputEmail" class="form-control" placeholder="Email address" required autofocus value="<?php if(isset($mail)) { echo $mail; } ?>">
     
-    <label for="inputPassword" class="sr-only">Password</label>
-    <input autocomplete="off" type="password" name="inputPassword" id="inputPassword" class="form-control" placeholder="Password" required autocomplete="off">
+    <label for="inputPassword" class="sr-only">Mot de passe</label>
+    <input autocomplete="off" type="password" name="inputPassword" id="inputPassword" class="form-control" placeholder="Password" required>
     
+    <!--afficher une petite liste deroulante-->
+    <label for="inputPrenom" class="sr-only">Civilité</label>
+    <input autocomplete="off" type="text" name="inputPrenom" id="inputPrenom" class="form-control" placeholder="Prénom" required value="<?php if(isset($prenom)) { echo $prenom; } ?>">
+
     <label for="inputNom" class="sr-only">Nom</label>
     <input autocomplete="off" type="text" name="inputNom" id="inputNom" class="form-control" placeholder="Nom" required value="<?php if(isset($nom)) { echo $nom; } ?>">
     
     <label for="inputPrenom" class="sr-only">Prenom</label>
     <input autocomplete="off" type="text" name="inputPrenom" id="inputPrenom" class="form-control" placeholder="Prénom" required value="<?php if(isset($prenom)) { echo $prenom; } ?>">
     
-    <div class="checkbox">
+    <label for="inputDateNaissance" class="sr-only">Date de naissance</label>
+    <input autocomplete="off" type="text" name="inputDateNaissance" id="inputDateNaissance" class="form-control" placeholder="Date de naisance" required value="<?php if(isset($prenom)) { echo $prenom; } ?>">
+
+    <label for="inputAdresse" class="sr-only">Adresse</label>
+    <input autocomplete="off" type="text" name="inputAdresse" id="inputAdresse" class="form-control" placeholder="Adresse" required value="<?php if(isset($prenom)) { echo $prenom; } ?>">
+
+    <label for="inputCodePostale" class="sr-only">Code Postale</label>
+    <input autocomplete="off" type="text" name="inputCodePostale" id="inputCodePostale" class="form-control" placeholder="CodePostale" required value="<?php if(isset($prenom)) { echo $prenom; } ?>">
+
+    <label for="inputVille" class="sr-only">Ville</label>
+    <input autocomplete="off" type="text" name="inputVille" id="inputVille" class="form-control" placeholder="Ville" required value="<?php if(isset($prenom)) { echo $prenom; } ?>">
+
+    <label for="inputNiveauEtude" class="sr-only">Niveau d'étude</label>
+    <input autocomplete="off" type="text" name="inputNiveauEtude" id="inputNiveauEtude" class="form-control" placeholder="Niveau d'étude" required value="<?php if(isset($prenom)) { echo $prenom; } ?>">
+
+    <label for="inputTypeContrat" class="sr-only">Type de Contrat</label>
+    <input autocomplete="off" type="text" name="inputTypeContrat" id="inputTypeContrat" class="form-control" placeholder="Type de contrat" required value="<?php if(isset($prenom)) { echo $prenom; } ?>">
+
+    <!-- <div class="checkbox">
       <label>
         <input type="checkbox" value="remember-me"> Remember me
       </label>
-    </div>
-    
+    </div> -->
+    <br />
     <button class="btn btn-lg btn-primary btn-bloick" name='forminscrip' value="inscription" type="submit">S'inscrire</button>
     <br/>
     <br/>
