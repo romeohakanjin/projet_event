54e0cd4e62a37a15b64bcc982e6befd404f79f27
Sindy-L <sindy.lim@outlook.fr>
2016-12-07 22:24:21 +0100

    Verification PARTIELS des champs inscription

--- public/js/inscription.js
+++ public/js/inscription.js
@@ -1,3 +1,47 @@
 $(function(){
 	$("#inputDateNaissance").datepicker();
 });
+
+function checkInscription(form) {
+    alert("Hello! I am an alert box!!");
+    var civilite = document.getElementById('civilite').value;
+    var nom = document.getElementById('inputNom').value;
+    var prenom = document.getElementById('inputPrenom').value;
+    var email = document.getElementById('inputEmail').value;
+    var niveau_etude = document.getElementById('niveau_etude').value;
+
+    var regexEmail = /^[_a-zA-Z0-9-]+(.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+.)+[a-zA-Z]{2,4}$/;
+    var message = "";
+
+    if(civilite == -1) {
+        alert("Veuillez saisir votre civilité svp!");
+        document.forms['form-inscription'].elements['civilite'].focus();
+    }
+    if(nom == "") {
+        alert("Veuillez saisir votre nom svp!");
+        document.forms['form-inscription'].elements['inputNom'].focus();
+    }
+    if(prenom == "") {
+        alert("Veuillez saisir votre prenom svp!");
+        document.forms['form-inscription'].elements['inputPrenom'].focus();
+    }
+    if(email == "") {
+        alert("Veuillez saisir votre email svp!");
+        document.forms['form-inscription'].elements['inputEmail'].focus();
+    }else if(regexEmail.test(email) == false){
+        alert("Veuillez saisir correctment votre email svp!");
+        document.forms['form-inscription'].elements['inputEmail'].focus();
+    }
+    if(niveau_etude == -1) {
+        alert("Veuillez saisir votre niveau d'étude svp!");
+        document.forms['form-inscription'].elements['niveau_etude'].focus();
+    }
+}
+
+function checkContrat(type_contrat){
+    var type_contrat = document.getElementById('type_contrat').value;
+    if(type_contrat == -1) {
+        alert("Veuillez saisir votre type de contrat svp!");
+        document.forms['form-inscription'].elements['type_contrat'].focus();
+    }
+}
--- views/users/inscription.php
+++ views/users/inscription.php
@@ -6,43 +6,54 @@
 <h2>Inscription</h2>
 <br />
 <div class="container">
-  <form class="form-signin" action="" method="POST">
+  <form class="form-signin" action="" method="POST" name="form-inscription">
     <!-- Civilité -->
-    <label for="inputCivilite">Civilité</label><br/>
-    <label><input type="checkbox" id="Checkbox" value="Mr"> Mr</label>
-    <input type="checkbox" id="Checkbox" value="Mme"> <label for="cbox2">Mme</label><br/>
+    <label for="civilite">Civilité</label><br/>
+    <select name="civilite" id="civilite" class="form-control" >
+        <option value="-1">Sélectionnez votre civilité</option>
+        <option value="Mr">Monsieur</option>
+        <option value="Mme">Madame</option>
+    </select>
 
     <!--  Nom  -->
     <label for="inputNom">Nom</label>
-    <input autocomplete="off" type="text" name="inputNom" id="inputNom" class="form-control" placeholder="Nom" required value="<?php if(isset($nom)) { echo $nom; } ?>">
-    
+    <input autocomplete="off" type="text" name="inputNom" id="inputNom" class="form-control" placeholder="Nom"  value="<?php if(isset($nom)) { echo $nom; } ?>">
+    </span class="info"></span>
+
     <!--  Prénom  -->
     <label for="inputPrenom">Prenom</label>
-    <input autocomplete="off" type="text" name="inputPrenom" id="inputPrenom" class="form-control" placeholder="Prénom" required value="<?php if(isset($prenom)) { echo $prenom; } ?>">
-    
+    <input autocomplete="off" type="text" name="inputPrenom" id="inputPrenom" class="form-control" placeholder="Prénom"  value="<?php if(isset($prenom)) { echo $prenom; } ?>">
+    </span class="info"></span>
+
     <!--  Date de naissance  -->
     <label for="inputDateNaissance">Date de naissance</label>
-    <input autocomplete="off" type="text" name="inputDateNaissance" id="inputDateNaissance" class="form-control" placeholder="Date de naisance" required >
+    <input autocomplete="off" type="text" name="inputDateNaissance" id="inputDateNaissance" class="form-control" placeholder="Date de naisance"  >
+    </span class="info"></span>
 
     <!--  Adresse  -->
     <label for="inputAdresse">Adresse</label>
-    <input autocomplete="off" type="text" name="inputAdresse" id="inputAdresse" class="form-control" placeholder="Adresse" required value="<?php if(isset($adresse)) { echo $adresse; } ?>">
+    <input autocomplete="off" type="text" name="inputAdresse" id="inputAdresse" class="form-control" placeholder="Adresse"  value="<?php if(isset($adresse)) { echo $adresse; } ?>">
+    </span class="info"></span>
 
     <!--  Code postale  -->
     <label for="inputCodePostale">Code Postale</label>
-    <input autocomplete="off" type="text" name="inputCodePostale" id="inputCodePostale" class="form-control" placeholder="CodePostale" required value="<?php if(isset($codePostale)) { echo $codePostale; } ?>">
+    <input autocomplete="off" type="text" name="inputCodePostale" id="inputCodePostale" class="form-control" placeholder="CodePostale"  value="<?php if(isset($codePostale)) { echo $codePostale; } ?>">
+    </span class="info"></span>
 
     <!--  Ville  -->
     <label for="inputVille">Ville</label>
-    <input autocomplete="off" type="text" name="inputVille" id="inputVille" class="form-control" placeholder="Ville" required value="<?php if(isset($ville)) { echo $ville; } ?>">
+    <input autocomplete="off" type="text" name="inputVille" id="inputVille" class="form-control" placeholder="Ville"  value="<?php if(isset($ville)) { echo $ville; } ?>">
+    </span class="info"></span>
 
     <!--  Email  -->
     <label for="inputEmail">Email</label>
-    <input autocomplete="off" type="email" name="inputEmail" id="inputEmail" class="form-control" placeholder="Adresse email" required autofocus value="<?php if(isset($mail)) { echo $mail; } ?>">
-    
+    <input autocomplete="off" type="email" name="inputEmail" id="inputEmail" class="form-control" placeholder="Adresse email" value="<?php if(isset($mail)) { echo $mail; } ?>">
+    </span class="info"></span>
+
     <!--  Niveau d'étude  -->
-    <label for="inputNiveauEtude">Niveau d'étude</label>
-    <select name="niveau_etude" id="niveau_etude" class="form-control" required>
+    <label for="niveau_etude">Niveau d'étude</label>
+    <select name="niveau_etude" id="niveau_etude" class="form-control" >
+        <option value="-1">Sélectionnez un niveau d'étude</option>
         <option value="1">Bac + 1</option>
         <option value="2">Bac + 2</option>
         <option value="3">Bac + 3</option>
@@ -50,20 +61,24 @@
         <option value="5">Bac + 5</option>
     </select>
     <br />
+    </span class="info"></span>
 
     <!--  Type de contrat  -->
-      <label for="inputTypeContrat">Type de Contrat</label>
-    <select name="type_contrat" id="type_contrat" class="form-control" required>
+    <label for="type_contrat">Type de Contrat</label>
+    <select name="type_contrat" id="type_contrat" class="form-control" onchange="checkContrat(this)">
+        <option value="-1">Sélectionnez un type de contrat</option>
         <option value="CDI">CDI</option>
         <option value="CDD">CDD</option>
         <option value="Alternance">Alternance</option>
         <option value="Stage">Stage</option>
     </select>
     <br />
-    <button class="btn btn-lg btn-primary btn-bloick" name='forminscrip' value="inscription" type="submit">S'inscrire</button>
+    </span class="info"></span>
+
+    <button class="btn btn-lg btn-primary btn-bloick" name='forminscrip' id='forminscrip' value="inscription" type="submit" onClick="checkInscription(this);">S'inscrire</button>
     <br/>
     <br/>
-  </form>
+</form>
 </div>
 
 
