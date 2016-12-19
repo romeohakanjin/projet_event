63d4ac446a99616db02fd17a41c3efec483e3c8b
Roméo Hakanjin <rhakanjin@Newsco.lan>
2016-12-19 14:24:36 +0100

    Modification redirection home et admin

--- admin.php
+++ admin.php
@@ -4,9 +4,9 @@ require_once __DIR__.'/api/vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
-/* Afficher la homepage */
-$app->get('/', function() use ($app) {
-    return $app->redirect('public/');
+/* Afficher la page admin */
+$app->get('/admin', function() use ($app) {
+    return $app->redirect('public/admin.php');
 });
 
 $app->run();
