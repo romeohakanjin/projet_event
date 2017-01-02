b3a0c70c02bd00816b2fa4af56fce42e74dfd004
Sindy-L <sindy.lim@outlook.fr>
2017-01-02 17:26:36 +0100

    suppression tout avant de mettre la nouvelle version

--- app/controllers/Membre_controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace App\controllers;
-
-    class Membre_controller extends Controller {
-        public function inscription($values){
-            var_dump("contrôle de saisie inscription ici");
-            var_dump($values);
-        }
-    }
