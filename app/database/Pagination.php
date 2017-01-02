ccb5c71c4e3c305c115b30d1781ae98b721ab939
romeohakanjin <romeo.hakanjin@hotmail.fr>
2017-01-02 20:30:23 +0100

    suppression fichier pagination dans database

--- app/database/Pagination.php
+++
@@ -1,8 +0,0 @@
-<?php
-    namespace App\models;
-    class Pagination extends \core\model\model{
-
-        public function getNombreMembre(){
-            return $this->db->query('SELECT COUNT AS nombre_membre (id) FROM membre ');
-        }
-    }
